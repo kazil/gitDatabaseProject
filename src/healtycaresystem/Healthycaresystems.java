fb4542b64e6e71c634217f990673704c9426d5ee
Andre Marklund <andmar82@gmail.com>
2013-03-01 11:47:24 +0100

    Added main method Added login

---
+++ src/healtycaresystem/Healthycaresystems.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package healtycaresystem;
+
+/**
+ * @author KazilLaptop
+ */
+class Healthycaresystems {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login window = new Login();
+        window.setVisible(true);
+
+    }
+}
+
